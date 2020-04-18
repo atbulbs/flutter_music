19a6b4c725c83c305e09cc2f4ebc581bee27c22c
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-04-18 23:24:00 +0800

    TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -10,35 +10,41 @@ class TabbedAppBarSample extends StatelessWidget {
           appBar: new AppBar(
             backgroundColor: Colors.red,
             title: const Text('Flutter Music'),
-            bottom: new TabBar(
-              indicatorSize: TabBarIndicatorSize.label,//指示器大小的计算方式，TabBarIndicatorSize.tab：跟每个tab等宽，TabBarIndicatorSize.label：跟文字等宽
-              indicatorColor: Colors.red,
-              labelColor: Colors.white,
-              unselectedLabelColor: Colors.black,
-              labelStyle: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w400,
-              ),
-              unselectedLabelStyle: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w400
-              ),
-              tabs: choices.map((Choice choice) {
-                return new Tab(
-                  text: choice.title,
+//            bottom:
+          ),
+          body: new Column(
+            children: <Widget>[
+              new TabBar(
+                indicatorSize: TabBarIndicatorSize.label,//指示器大小的计算方式，TabBarIndicatorSize.tab：跟每个tab等宽，TabBarIndicatorSize.label：跟文字等宽
+                indicatorColor: Colors.red,
+                labelColor: Colors.red,
+                unselectedLabelColor: Colors.black,
+                labelStyle: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+                unselectedLabelStyle: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400
+                ),
+                tabs: choices.map((Choice choice) {
+                  return new Tab(
+                    text: choice.title,
 //                  icon: new Icon(choice.icon),
-                );
-              }).toList(),
-            ),
-          ),
-          body: new TabBarView(
-            children: choices.map((Choice choice) {
-              return new Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: new ChoiceCard(choice: choice),
-              );
-            }).toList(),
-          ),
+                  );
+                }).toList(),
+              ),
+//              TabBarView(
+//                children: choices.map((Choice choice) {
+//                  return new Padding(
+//                    padding: const EdgeInsets.all(16.0),
+//                    child: new ChoiceCard(choice: choice),
+//                  );
+//                }).toList(),
+//              ),
+            ],
+
+          )
         ),
       ),
     );
@@ -69,8 +75,8 @@ class ChoiceCard extends StatelessWidget {
       color: Colors.white,
       child: new Center(
         child: new Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+//          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
 //            new Icon(choice.icon, size: 128.0, color: textStyle.color),
             new Text(choice.title, style: textStyle),
